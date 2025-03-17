c6f88718d6240120129d230d9ff1f3dc205072f5
paualberti <paualberti73@gmail.com>
2025-03-18 00:55:32 +0100

    so many changes

--- config/.config/nvim/lua/paualberti/plugins/screenkey.lua
+++ config/.config/nvim/lua/paualberti/plugins/screenkey.lua
@@ -1,14 +1,8 @@
 return {
 	"nvzone/showkeys",
-	event = "VeryLazy",
 	opts = {
 		position = "top-right",
 		maxkeys = 4,
 	},
-	config = function(_, opts)
-		local showkeys = require("showkeys")
-		showkeys.setup(opts)
-		showkeys.open()
-	end,
 	keys = { { "<leader>uS", "<Cmd>ShowkeysToggle<CR>", desc = "ShowkeysToggle" } },
 }
