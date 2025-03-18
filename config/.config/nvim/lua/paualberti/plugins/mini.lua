9a80d12b48317ac5d7efbdc6f869ad4490ba1f74
paualberti <paualberti73@gmail.com>
2025-03-18 13:16:52 +0100

    removed many mini plugins

--- config/.config/nvim/lua/paualberti/plugins/mini.lua
+++ config/.config/nvim/lua/paualberti/plugins/mini.lua
@@ -7,27 +7,15 @@ return {
 		opts = { mappings = { start_with_preview = "" } },
 	},
 	{ "echasnovski/mini.comment", event = "VeryLazy", opts = {} },
-	{ "echasnovski/mini.completion", opts = {} },
-	{ "echasnovski/mini.move", opts = {} },
-	{ "echasnovski/mini.operators", opts = {} },
 	{ "echasnovski/mini.pairs", event = "InsertEnter", opts = { mappings = { ["`"] = false } } },
-	{ "echasnovski/mini.snippets", opts = {} },
 	{ "echasnovski/mini.splitjoin", event = "VeryLazy", opts = {} },
 	{ "echasnovski/mini.surround", event = "VeryLazy", opts = {} },
 
 	-- General workflow
-	{ "echasnovski/mini.basics", opts = {} },
 	{ "echasnovski/mini.bracketed", event = "VeryLazy", opts = {} },
 	{ "echasnovski/mini.bufremove", opts = {} },
-	{ "echasnovski/mini.clue", opts = {} },
 	{ "echasnovski/mini.diff", opts = {} },
 	{ "echasnovski/mini.extra", opts = {} },
-	{ "echasnovski/mini.files", opts = {} },
-	-- { "echasnovski/mini-git", opts = {} },
-	{ "echasnovski/mini.jump", opts = {} },
-	{ "echasnovski/mini.jump2d", opts = {} },
-	{ "echasnovski/mini.misc", opts = {} },
-	{ "echasnovski/mini.pick", opts = {} },
 	{
 		"echasnovski/mini.sessions",
 		opts = {},
@@ -41,7 +29,6 @@ return {
 	-- Appearance
 	{ "echasnovski/mini.animate", opts = { scroll = { enable = false }, resize = { enable = false } } },
 	-- { "echasnovski/mini.base16", opts = {} },
-	{ "echasnovski/mini.colors", opts = {} },
 	{ "echasnovski/mini.cursorword", opts = {} },
 	{
 		"echasnovski/mini.hipatterns",
@@ -62,15 +49,9 @@ return {
 			})
 		end,
 	},
-	{ "echasnovski/mini.hues", opts = { background = "#002734", foreground = "#c0c8cc" } },
-	{ "echasnovski/mini.icons", opts = {} },
-	{ "echasnovski/mini.indentscope", opts = {} },
-	{ "echasnovski/mini.map", opts = {} },
 	{ "echasnovski/mini.notify", event = "VeryLazy", opts = {} },
-	{ "echasnovski/mini.starter", opts = {} },
 	{ "echasnovski/mini.statusline", opts = {} },
 	{ "echasnovski/mini.tabline", opts = {} },
-	{ "echasnovski/mini.trailspace", opts = {} },
 
 	-- Other
 	{ "echasnovski/mini.doc", opts = {} },
--- config/.config/nvim/lua/paualberti/plugins/which-key.lua
+++ config/.config/nvim/lua/paualberti/plugins/which-key.lua
@@ -1,7 +1,7 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		delay = 500,
 		icons = {
