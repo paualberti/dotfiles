6dc47417a27be92132aed061ed6284862cffde59
paualberti <paualberti73@gmail.com>
2025-03-19 15:23:31 +0100

    paualberti/toggleterm

--- config/.config/nvim/lua/paualberti/plugins/toggleterm.lua
+++ config/.config/nvim/lua/paualberti/plugins/toggleterm.lua
@@ -1,15 +1,11 @@
 return {
-	dir = "~/personal/toggleterm.nvim",
+	"paualberti/toggleterm.nvim",
 	version = "*",
 	event = "VeryLazy",
 	opts = {
 		open_mapping = "<C-t>",
 		direction = "float",
-		float_opts = {
-			padding = 2,
-			max_width = math.floor(0.8 * vim.o.columns),
-			max_height = math.floor(0.9 * vim.o.lines),
-		},
+		float_opts = { padding = 2 },
 	},
 	keys = {},
 }
