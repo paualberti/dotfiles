3e355a187fae38375306097ac23ecb75e1a2df30
paualberti <paualberti73@gmail.com>
2025-03-23 22:59:02 +0100

    No need for preview picker

--- config/.config/nvim/lua/paualberti/plugins/live-preview.lua
+++ config/.config/nvim/lua/paualberti/plugins/live-preview.lua
@@ -1,14 +1,12 @@
 return {
 	"brianhuster/live-preview.nvim",
 	dependencies = {
-		-- You can choose one of the following pickers
-		"ibhagwan/fzf-lua",
+		-- "ibhagwan/fzf-lua",
 	},
 	ft = "markdown",
 	keys = {
 		{ "<leader>ms", "<Cmd>LivePreview start<CR>", desc = "Start" },
 		{ "<leader>mc", "<Cmd>LivePreview close<CR>", desc = "Close" },
-		{ "<leader>mp", "<Cmd>LivePreview pick<CR>", desc = "Pick" },
 	},
 	config = function()
 		local aug = vim.api.nvim_create_augroup("LivePreview", { clear = true })
