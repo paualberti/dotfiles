afec639b50089b433a515203a3182e146ef3be95
paualberti <paualberti73@gmail.com>
2025-03-24 02:02:59 +0100

    <C-t> to toggle term

--- config/.config/nvim/lua/paualberti/plugins/toggleterm.lua
+++ config/.config/nvim/lua/paualberti/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
 	tag = "v0.1.0",
 	opts = {},
 	keys = {
-		{ "<leader>to", "<Cmd>ToggleTerm<CR>", desc = "Open" },
+		{ "<C-t>", "<Cmd>ToggleTerm<CR>", desc = "Open" },
 		{ "<leader>tc", "<Cmd>TermClose<CR>", desc = "close" },
 	},
 }
