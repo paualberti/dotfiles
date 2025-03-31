d9d360b6eb0c0d608500c07bf2f42b1cbc131889
paualberti <paualberti73@gmail.com>
2025-03-31 12:44:33 +0200

    mini.surround with 'S'

--- config/.config/nvim/lua/paualberti/plugins/mini.lua
+++ config/.config/nvim/lua/paualberti/plugins/mini.lua
@@ -9,19 +9,19 @@ return {
 		"echasnovski/mini.surround",
 		opts = {
 			mappings = {
-				add = "gsa", -- Add surrounding in Normal and Visual modes
-				delete = "gsd", -- Delete surrounding
-				find = "gsf", -- Find surrounding (to the right)
-				find_left = "gsF", -- Find surrounding (to the left)
-				highlight = "gsh", -- Highlight surrounding
-				replace = "gsr", -- Replace surrounding
-				update_n_lines = "gsn", -- Update `n_lines`
+				add = "Sa", -- Add surrounding in Normal and Visual modes
+				delete = "Sd", -- Delete surrounding
+				find = "Sf", -- Find surrounding (to the right)
+				find_left = "SF", -- Find surrounding (to the left)
+				highlight = "Sh", -- Highlight surrounding
+				replace = "Sr", -- Replace surrounding
+				update_n_lines = "Sn", -- Update `n_lines`
 
 				suffix_last = "l", -- Suffix to search with "prev" method
 				suffix_next = "n", -- Suffix to search with "next" method
 			},
 		},
-		keys = { "gs" },
+		keys = { "S" },
 	},
 
 	-- General workflow
