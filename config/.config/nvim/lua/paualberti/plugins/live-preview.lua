9c7b289ad404dcf4fb0d3f8c84882b46899517d1
paualberti <paualberti73@gmail.com>
2025-04-01 12:00:01 +0200

    don't load until keys

--- config/.config/nvim/lua/paualberti/plugins/live-preview.lua
+++ config/.config/nvim/lua/paualberti/plugins/live-preview.lua
@@ -1,16 +1,11 @@
 return {
 	"brianhuster/live-preview.nvim",
-	dependencies = {
-		-- "ibhagwan/fzf-lua",
-	},
-	ft = "markdown",
 	keys = {
 		{ "<leader>ms", "<Cmd>LivePreview start<CR>", desc = "LivePreview Start" },
 		{ "<leader>mc", "<Cmd>LivePreview close<CR>", desc = "LivePreview Close" },
 	},
 	config = function()
 		local aug = vim.api.nvim_create_augroup("LivePreview", { clear = true })
-
 		vim.api.nvim_create_autocmd("VimLeave", {
 			pattern = "*.md",
 			callback = function()
--- config/.config/nvim/lua/paualberti/plugins/render-markdown.lua
+++ config/.config/nvim/lua/paualberti/plugins/render-markdown.lua
@@ -1,8 +1,6 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-	ft = "markdown",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
 	opts = {},
